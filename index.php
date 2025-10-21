604b5b5edd5c1c167320e055c073b4e2c7339969
shpak2207b <shpaktp@mer.ci.nsu.ru>
2025-10-22 00:55:49 +0700

    не работает апач

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-include "functions.php";
-$db = include "database/start.php";
-$posts = $db->getAll("posts");
+include __DIR__ . "/../functions.php";
 
-include "index.view.php";
+$routes = [
+    "/home" => __DIR__ . '/../homepage.php',
+    "/about" => __DIR__ . '/../about.php'
+];
+$route = $_SERVER['REQUEST_URI'];
+if(array_key_exists($route, $routes)) {
+    include($routes[$route]); exit();
+}
+else {
+    dd(404);
+}
 
 ?>
